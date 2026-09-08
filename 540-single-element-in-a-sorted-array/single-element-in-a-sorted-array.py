7e5de202569df86453f234d765ab2f7eb98a63a0
Lohith Gowda C P <lohithgowda062004@gmail.com>
2026-09-08 23:16:32 +0530

    Time: 0 ms (100.00%) | Memory: 26.9 MB (91.75%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -17,13 +17,13 @@ class Solution:
         while low <= high:
             mid = (low+high) // 2
 
-            if nums[mid] != nums[mid+1] and nums[mid] != nums[mid-1]:
+            if nums[mid] != nums[mid-1] and nums[mid] != nums[mid+1]:
                 return nums[mid]
-
-            if (mid%2 == 1 and nums[mid] == nums[mid-1]) or\
-                (mid%2 == 0 and nums[mid] == nums[mid+1]):
+            
+            if (mid % 2 == 1 and nums[mid] == nums[mid-1]) or\
+                (mid % 2 == 0 and nums[mid] == nums[mid+1]):
                 low = mid + 1
             else:
                 high = mid - 1
-        
+
         return -1
